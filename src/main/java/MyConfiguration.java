54120553dada92e3c62bf4c7e1f8486af6639b74
Ian Flanagan <ianianf@yahoo.com>
2020-09-24 16:43:36 -0500

    fixed config file.

--- src/main/java/MyConfiguration.java
+++ src/main/java/MyConfiguration.java
@@ -1,6 +1,6 @@
 public class MyConfiguration {
 
-    public static final String myAccessToken = "fdc33dfd685543c09aa4cd6afc98980d"; // fdc33dfd685543c09aa4cd6afc98980d <post_server_item>
+    public static final String myAccessToken = "<post_server_item>"; // found in rollbar UI
     public static final String environment = "Prod";
     public static final String environmentDEV = "DEV";
     public static final String environmentUAT = "UAT";
